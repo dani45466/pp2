3953c0ebc5892e4009d2fa4c95ebbdec9d1cf1af
Alan Daniyarov <a_daniyarov@kbtu.kz>
2026-01-26 22:34:35 +0500

    string_example

---
+++ Practice1/python-basics/strings.py
@@ -0,0 +1,14 @@
+# Создаём строковые переменные
+name = "Алиса"
+city = 'Москва'
+message = "Привет, мир!"
+
+# Выводим значения
+print(name)
+print(city)
+print(message)
+
+# Проверяем тип переменных
+print(type(name))
+print(type(city))
+print(type(message))
